9f7e56209dd983303d645082b67b94d2571a55af
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-24 01:43:52 +0400

    more comments

--- Java-skills-Qais/src/ShoppingCart.java
+++ Java-skills-Qais/src/ShoppingCart.java
@@ -17,14 +17,14 @@ public class ShoppingCart {
                 total = total*0.8;
                 break;
             default:
-                System.out.println("invalid customer type");
+                System.out.println("invalid customer type"); //ensure only accepted customer types
         }
         return total;
     }
 
     public static double findMostExpensive (double[] prices){
         if (prices.length==0){
-            System.out.println("invalid prices");
+            System.out.println("invalid prices"); //handle non-zero
             return 0;
         }
         double mostExpensive = prices[0];
@@ -39,7 +39,7 @@ public class ShoppingCart {
     public static int countExpensiveItems(double[] prices, double threshold){
         int count = 0;
         for(int i=0;i<prices.length;i++){
-            if(prices[i]>threshold){
+            if(prices[i]>threshold){ //increment any time a price crosses the threshold
                 count++;
             }
         }
