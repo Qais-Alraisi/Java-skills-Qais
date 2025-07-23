d54826f52e059ce9097ab77103f06ac128b394df
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-23 23:49:47 +0400

    finished task 5.1 - added testing

--- Java-skills-Qais/src/Main.java
+++ Java-skills-Qais/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
-
+        double[] cart = {25.99, 45.50, 12.99, 89.99, 15.75};
+        System.out.println("Regular customer total: $" + ShoppingCart.calculateTotal(cart,
+                "REGULAR"));
+        System.out.println("Premium customer total: $" + ShoppingCart.calculateTotal(cart,
+                "PREMIUM"));
+        System.out.println("VIP customer total: $" + ShoppingCart.calculateTotal(cart,
+                "VIP"));
+        System.out.println("The most expensive item is: "+ShoppingCart.findMostExpensive(cart));
+        System.out.println("The number of items in cart over 30$ are: "+ShoppingCart.countExpensiveItems(cart,30));
     }
 }
