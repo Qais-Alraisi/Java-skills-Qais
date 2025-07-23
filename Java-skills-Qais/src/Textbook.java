86f045f77b87c48197ba7655542df4fff77fc86a
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-23 21:33:02 +0400

    created Textbook child class

--- Java-skills-Qais/src/Textbook.java
+++ Java-skills-Qais/src/Textbook.java
@@ -1,2 +1,26 @@
-public class Textbook {
+public class Textbook extends Book{
+    private String _subject;
+    private int _edition;
+
+    public Textbook(String title, String author, int pages,Boolean isAvailable, String subject, int edition) {
+        super(title, author, pages, isAvailable);
+        _subject = subject;
+        _edition = edition;
+    }
+
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Subject: " + _subject);
+        System.out.println("Edition: " + _edition);
+    }
+
+    public String getSubject(){
+        return _subject;
+    }
+
+    public int getEdition(){
+        return _edition;
+    }
+
 }
