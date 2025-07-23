0ea30d507c3c9183625006ba1797664171018cdf
Qais Al Raisi <23-0622@student.gutech.edu.om>
2025-07-23 21:43:55 +0400

    added testing

--- Java-skills-Qais/src/Main.java
+++ Java-skills-Qais/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Book book1 = new Book("The Hunger Games","Suzanne Collins", 192, true);
+        Book book2 = new Book("Diary of a wimpy kid","Jeff Kinney",231,false);
+        Textbook textbook1 = new Textbook("Calculus 1","Dr. pete",452,false,"Mathematics",3);
+        book1.borrowBook();
+        textbook1.borrowBook();
+
+        book2.returnBook();
+
+        book1.displayInfo();
+        textbook1.displayInfo();
+
+        System.out.println(book1.getPages());
+
 
     }
 }
